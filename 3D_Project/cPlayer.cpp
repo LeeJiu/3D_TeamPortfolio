1258fc2f01448d28ecd2f65cdb5916a5a10a2d7c
LeeJiu <leeji0408@skhu.kr>
2016-11-11 20:09:04 +0900

    사용하지 않는 파일 삭제
    필터도 정리 해놓음. 필터 깨지면 압축된 파일 풀어서 사용할 것.

--- 3D_Project/cPlayer.cpp
+++ 3D_Project/cPlayer.cpp
@@ -7,7 +7,7 @@
 
 cPlayer::cPlayer()
 {
-	
+
 }
 
 
@@ -24,8 +24,6 @@ void cPlayer::BaseObjectEnable()
 
 	//캐릭터의 그려진 위치를 세팅
 	pTransform->SetWorldPosition(0, pTerrain->GetHeight(0, 0), 0);
-
-	m_nIndex = 0;
 }
 
 void cPlayer::BaseObjectUpdate(float timeDelta)
@@ -42,94 +40,9 @@ void cPlayer::BaseObjectUpdate(float timeDelta)
 		//m_pInput->DeleteKey('1');
 		m_pInput->SwapKey('1', '2');
 	}
-
-	KeyControl(timeDelta);
-
-	MovePoint(timeDelta);
 }
 
 void cPlayer::BaseObjectRender()
 {
 	this->pSkinned->Render(this->pTransform);
-
-	for (m_viWayPoint = m_vWayPoint.begin(); m_viWayPoint != m_vWayPoint.end(); ++m_viWayPoint)
-	{
-		GIZMO_MGR->WireSphere(D3DXVECTOR3(m_viWayPoint->x, m_viWayPoint->y, m_viWayPoint->z), 0.5f);
-	}
-}
-
-void cPlayer::KeyControl(float timeDelta)
-{
-	if (KEY_MGR->IsOnceDown(VK_LBUTTON))
-	{
-		//HEAD
-		//if (m_state != AS_WALK)
-		//{
-		//	m_state = AS_WALK;;;;
-		//	m_pState->EnterState(m_state, 'E');
-		//}
-	
-		Ray ray;
-		POINT ptMousePos = GetMousePos();
-		D3DXVECTOR2 screenPos(ptMousePos.x, ptMousePos.y);
-		m_camera->ComputeRay(&ray, &screenPos);
-
-		D3DXVECTOR3 pos;
-		pTerrain->IsIntersectRay(&pos, &ray);
-
-		m_vWayPoint.push_back(pos);
-	}
-
-	if (KEY_MGR->IsOnceDown(VK_SPACE))
-	{
-		m_bMove = true;
-	}
-}
-
-void cPlayer::MovePoint(float timeDelta)
-{
-	if (m_bMove == true)
-	{
-		D3DXVECTOR3 wayPoint(m_vWayPoint[m_nIndex].x, m_vWayPoint[m_nIndex].y, m_vWayPoint[m_nIndex].z);
-
-		//거리를 구한다.
-		D3DXVECTOR3 dirToTarget = wayPoint - this->pTransform->GetWorldPosition();
-		float dist = D3DXVec3Length(&dirToTarget);
-
-		//히트 포인트에 위치하게 되면 lookdirection을 하지 않는다.
-		//자신이 자신의 위치를 보게 되면 모델이 사라져버린다.
-		//다음 위치로 이동하게 만들자.
-		if (dist <= 0.001)
-		{
-			if (m_nIndex < m_vWayPoint.size() - 1)
-				m_nIndex++;
-			else if(m_nIndex == m_vWayPoint.size() - 1)
-				m_bMove = false;
-			return;
-		}
-
-
-		//방향을 구한다.
-		this->pTransform->LookDirection(dirToTarget);
-
-		//이동량
-		float deltaMove = 5.0f * timeDelta;
-		float t = Clamp01(deltaMove / dist);
-
-	if (KEY_MGR->IsOnceDown('P'))
-	{
-		//m_pInput->ChangeKey('3', new cTestCommand);	//없으면 추가된다.
-		//m_pInput->DeleteKey('1');
-		m_pInput->SwapKey('1', '2');
-	}
-	//if(m_pMove)
-	//	m_pMove->update(timeDelta, NULL);
-	//	//현재 위치에서 웨이 포인트로
-	//	D3DXVECTOR3 pos = VecLerp(this->pTransform->GetWorldPosition(), wayPoint, t);
-	//
-	//	//높이 얻는다. / 터레인의 높이
-	//	pos.y = this->pTerrain->GetHeight(pos.x, pos.z);
-	//
-	//	this->pTransform->SetWorldPosition(pos);
-	}
 }
